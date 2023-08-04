174b6722db2cece689adac53b2daedc40e9fa9d9
liujl <9347428+jinglong-liu@user.noreply.gitee.com>
2023-08-04 17:03:08 +0800

    增加单元测试代码

--- src/test/java/com/example/test_rabbitmq/TestRabbitmqApplicationTests.java
+++ src/test/java/com/example/test_rabbitmq/TestRabbitmqApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.test_rabbitmq;
 
+import com.example.test_rabbitmq.utils.RabbitMqUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @SpringBootTest
 class TestRabbitmqApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	void testSendDelayMessage() throws Exception {
+		Map<String, Object> map = new HashMap<>(2);
+		map.put("orderId", System.currentTimeMillis());
+		// 发送消息 延迟2秒
+		RabbitMqUtils.sendDelayMessage(rabbitTemplate, 2000, objectMapper.writeValueAsString(map), System.currentTimeMillis());
+		log.info("======>>>>>>发送延迟队列消息成功！消息内容：{}", map);
+	}
 }
